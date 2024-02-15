3c3a428304e22868c7005d60a1a4db5916cea640
뚱이 <djjang0925@gmail.com>
2024-02-16 01:50:15 +0900

    Refactor: 병합 완료

--- Frontend/malitell/src/styles/article/articleDetail/commentList.ts
+++ Frontend/malitell/src/styles/article/articleDetail/commentList.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
 width: 100%;
 min-height: 50px;
-margin-top: 20px;
+margin: 20px 0;
 background-color: white;
 border-radius: 10px;
 box-shadow: 4px 4px 4px rgba(0, 0, 0, 0.3);
